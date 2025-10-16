ca14bc27658c8cf62e599bd21a1e16c6b959eb84
Evgenii Kin <loli.timkin@yandex.ru>
2025-10-16 20:49:16 +0300

    login + signup pages

--- src/app/views/user/signup/signup.component.ts
+++ src/app/views/user/signup/signup.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.scss'],
   styles: [
   ]
 })
