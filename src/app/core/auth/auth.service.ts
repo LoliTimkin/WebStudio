40d4452327f1bba81cdcfa7a5a4721a96d258e26
Evgenii Kin <loli.timkin@yandex.ru>
2025-10-21 18:58:09 +0300

    работа с токенами в сервисе

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-import {Observable} from "rxjs";
+import {Observable, Subject} from "rxjs";
 import {DefaultResponseType} from "../../../types/default-response.type";
 import {LoginResponseType} from "../../../types/login-response.type";
 
@@ -9,11 +9,62 @@ import {LoginResponseType} from "../../../types/login-response.type";
 })
 export class AuthService {
 
+  public accessTokenKey: string = 'accessToken';
+  public refreshTokenKey: string = 'refreshToken';
+  public userIdKey: string = 'userId';
 
-  constructor(private http: HttpClient) { }
+  public isLogged$: Subject<boolean> = new Subject<boolean>();
+
+  private isLogged: boolean = false;
+
+  constructor(private http: HttpClient) {
+    this.isLogged = !!localStorage.getItem(this.accessTokenKey);
+  }
 
   login(email: string, password: string, rememberMe: boolean): Observable<LoginResponseType | DefaultResponseType> {
     return this.http.post<LoginResponseType | DefaultResponseType>('localhost:3000/auth/login',
       {email, password, rememberMe})
   }
+
+  public getIsLoginIn(): boolean {
+    return this.isLogged
+  }
+
+  public setTokens(accessToken: string, refreshToken: string) {
+    localStorage.setItem(this.accessTokenKey, accessToken)
+    localStorage.setItem(this.refreshTokenKey, refreshToken)
+    this.isLogged = true;
+    this.isLogged$.next(true)
+  }
+
+  public removeTokens(){
+    localStorage.removeItem(this.accessTokenKey)
+    localStorage.removeItem(this.refreshTokenKey)
+    this.isLogged = false
+    this.isLogged$.next(false)
+  }
+
+  public getTokens(): {accessToken: string | null, refreshToken: string | null} {
+    return {
+      accessToken: localStorage.getItem(this.accessTokenKey),
+      refreshToken: localStorage.getItem(this.refreshTokenKey)
+    }
+  }
+
+  set userId(id: string | null) {
+    if(id) {
+      localStorage.setItem(this.userIdKey, id)
+    } else {
+      localStorage.removeItem(this.userIdKey)
+    }
+
+  }
+
+  get userId(): string | null {
+      return localStorage.getItem(this.userIdKey)
+  }
+
+
+
+
 }
